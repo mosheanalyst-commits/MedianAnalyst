d6d445c9f9f750ff148770721c452593cc2ef576
Tools-4all <toolsfor4all@gmail.com>
2026-06-21 19:31:01 +0300

    Fix routing for custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
 )
-
